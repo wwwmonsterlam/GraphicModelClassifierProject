223ce08bc113cf36fd6e85ef9f9b6788ba59b935
wwwmonsterlam <wuweilin.william@qq.com>
2017-11-01 16:34:24 -0300

    initial commit

---
+++ src/main/java/priv/weilinwu/graphicmodel/App.java
@@ -0,0 +1,18 @@
+package priv.weilinwu.graphicmodel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static final Logger logger = LoggerFactory.getLogger(App.class);
+	
+    public static void main( String[] args )
+    {
+        logger.info( "Hello World!" );
+    }
+}
