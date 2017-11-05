e3a566b02beb1475f287dcf922d77aed3c15ece4
wwwmonsterlam <wuweilin.william@qq.com>
2017-11-05 01:06:42 -0400

    get training set and testing set

---
+++ src/main/java/priv/weilinwu/graphicmodel/Main.java
@@ -0,0 +1,28 @@
+package priv.weilinwu.graphicmodel;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main 
+{
+	public static final Logger logger = LoggerFactory.getLogger(Main.class);
+	
+	private final static String RESOURCE_PATH = "src/main/resources/";
+	private final static String[] DATA_FILE_NAMES = {"classify_d3_k2_saved1.mat",
+			"classify_d3_k2_saved2.mat", "classify_d3_k2_saved3.mat",
+			"classify_d4_k3_saved1.mat", "classify_d4_k3_saved2.mat",
+			"classify_d5_k3_saved1.mat", "classify_d5_k3_saved2.mat",
+			"classify_d99_k50_saved1.mat", "classify_d99_k50_saved2.mat",
+			"classify_d99_k60_saved1.mat", "classify_d99_k60_saved2.mat"};
+	
+    public static void main( String[] args ) throws IOException
+    {
+    	for(int i = 0; i < DATA_FILE_NAMES.length; i++) {
+        	Utils.getTrainingSet(Utils.dataLoader(RESOURCE_PATH + DATA_FILE_NAMES[i]));
+        	Utils.getTestingSet(Utils.dataLoader(RESOURCE_PATH + DATA_FILE_NAMES[i]));
+    	}
+    	
+    }
+}
---
+++ src/main/java/priv/weilinwu/graphicmodel/Utils.java
@@ -0,0 +1,57 @@
+package priv.weilinwu.graphicmodel;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.ujmp.core.Matrix;
+import org.ujmp.core.calculation.Calculation.Ret;
+import org.ujmp.jmatio.ImportMatrixMAT;
+
+public class Utils {
+	public static final Logger logger = LoggerFactory.getLogger(Utils.class);
+	
+	public static Matrix[] dataLoader(String dataPath) throws IOException {
+		File file = new File(dataPath);
+		Matrix[] matrixs = new Matrix[2];
+		matrixs[0] = ImportMatrixMAT.fromFile(file, "class_1");
+		matrixs[1] = ImportMatrixMAT.fromFile(file, "class_2");
+		
+		logger.debug("The size of the matrix is " + matrixs[0].getRowCount() + " by " 
+				+ matrixs[0].getColumnCount());
+//		logger.debug(matrixs[0].toString());
+//		logger.debug(matrixs[1].toString());
+		
+		return matrixs;
+	}
+	
+	public static Matrix[] getTrainingSet(Matrix[] matrixs) {
+		long rowCount = matrixs[0].getRowCount();
+		long columnCount = matrixs[0].getColumnCount();	
+		Matrix[] trainingSet = new Matrix[2];
+		
+		trainingSet[0] = matrixs[0].subMatrix(Ret.NEW, 0, 0, rowCount - 1, columnCount / 4 * 3 - 1);
+		trainingSet[1] = matrixs[1].subMatrix(Ret.NEW, 0, 0, rowCount - 1, columnCount / 4 * 3 - 1);
+		
+		logger.debug("The size of training set is: " + trainingSet[0].getRowCount() + 
+				" by " + trainingSet[0].getColumnCount());
+		
+		return trainingSet;
+	}
+	
+	public static Matrix[] getTestingSet(Matrix[] matrixs) {
+		long rowCount = matrixs[0].getRowCount();
+		long columnCount = matrixs[0].getColumnCount();	
+		Matrix[] testingSet = new Matrix[2];
+		
+		testingSet[0] = matrixs[0].subMatrix(Ret.NEW, 0, columnCount / 4 * 3, rowCount - 1, columnCount - 1);
+		testingSet[1] = matrixs[1].subMatrix(Ret.NEW, 0, columnCount / 4 * 3, rowCount - 1, columnCount - 1);
+		
+		logger.debug("The size of testing set is: " + testingSet[0].getRowCount() + 
+				" by " + testingSet[0].getColumnCount());
+		
+		return testingSet;
+		
+	}
+}
