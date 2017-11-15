fc7b95fa75bed6f370ecafa60d1763e5dddb70a2
wwwmonsterlam <wuweilin.william@qq.com>
2017-11-15 05:04:07 -0500

    remove presentation files

--- src/main/java/priv/weilinwu/graphicmodel/Main.java
+++ src/main/java/priv/weilinwu/graphicmodel/Main.java
@@ -20,26 +20,28 @@ public class Main
 	
     public static void main( String[] args ) throws IOException
     {
-    	char choice = getUserChoice();
-    	int iterationCount = choice == 'a'? 1 : 100; 
-    	double[] accuracies = new double[DATA_FILE_NAMES.length];
-    	for(int i = 0; i < DATA_FILE_NAMES.length; i++) {
-    		
-    		String filePath = RESOURCE_PATH + DATA_FILE_NAMES[i];
-    		Matrix[] dataSet = Utils.dataLoader(filePath);
-    		Utils.scaleData(dataSet);
-	    	Matrix[] trainingSet = Utils.getTrainingSet(dataSet);
-	    	Matrix[] testingSet = Utils.getTestingSet(dataSet);
+    	while(true) {
+	    	char choice = getUserChoice();
+	    	int iterationCount = choice == 'a'? 1 : 100; 
+	    	double[] accuracies = new double[DATA_FILE_NAMES.length];
+	    	for(int i = 0; i < DATA_FILE_NAMES.length; i++) {
+	    		
+	    		String filePath = RESOURCE_PATH + DATA_FILE_NAMES[i];
+	    		Matrix[] dataSet = Utils.dataLoader(filePath);
+	    		Utils.scaleData(dataSet);
+		    	Matrix[] trainingSet = Utils.getTrainingSet(dataSet);
+		    	Matrix[] testingSet = Utils.getTestingSet(dataSet);
+		    	
+		    	LogisticClassifier logisticClassifier = new LogisticClassifier(trainingSet, testingSet);
+		    	
+		    	logger.debug("Running logistic classifier using dataset {" + filePath + "} :");
+		    	accuracies[i] = logisticClassifier.getCorrectionRateUsingTestingSetUsingMultipleRandomStartingPoint(iterationCount);
+		    	logger.debug("######################################################################################");
+		    	logger.debug("correction rate (" + DATA_FILE_NAMES[i] + "): " + accuracies[i] + "\n");
+	    	}
 	    	
-	    	LogisticClassifier logisticClassifier = new LogisticClassifier(trainingSet, testingSet);
-	    	
-	    	logger.debug("Running logistic classifier using dataset {" + filePath + "} :");
-	    	accuracies[i] = logisticClassifier.getCorrectionRateUsingTestingSetUsingMultipleRandomStartingPoint(iterationCount);
-	    	logger.debug("######################################################################################");
-	    	logger.debug("correction rate (" + DATA_FILE_NAMES[i] + "): " + accuracies[i] + "\n");
+	    	summary(accuracies);
     	}
-    	
-    	summary(accuracies);
     }
     
     public static void summary(double[] accuracies) {
@@ -50,6 +52,8 @@ public class Main
     	for(int i = 0; i < DATA_FILE_NAMES.length; i++) {
 	    	logger.debug("correction rate (" + DATA_FILE_NAMES[i] + "): " + accuracies[i]);
     	}
+    	
+    	logger.debug("\n\n");
     }
     
     public static char getUserChoice() throws IOException {
